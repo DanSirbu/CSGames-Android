b2ac4d1c90f7b5792287475b33807bc41d95c52a
Dan Sirbu <dsirb066@gmail.com>
2019-03-23 14:17:10 -0400

    Added api models

--- android/app/src/main/java/com/csgames/mixparadise/api/Api.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/Api.kt
@@ -2,7 +2,9 @@ package com.csgames.mixparadise.api
 
 import android.content.Context
 import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
@@ -13,6 +15,14 @@ object Api {
     fun init(context: Context) {
         val okHttpClient = OkHttpClient.Builder()
             .cache(Cache(File(context.cacheDir, "http-cache"), 10 * 1024 * 1024))
+            .addInterceptor(
+                object : Interceptor {
+                    override fun intercept(chain: Interceptor.Chain): Response {
+                        val request = chain.request().newBuilder().addHeader("Team", "Citation Needed").build()
+                        return chain.proceed(request)
+                    }
+                }
+            )
             .build()
 
         retrofit = Retrofit.Builder()
--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -1,7 +1,26 @@
 package com.csgames.mixparadise.api
 
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+
 interface DrinkService {
     // TODO: get ingredients
 
+    @GET("ingredients")
+    fun getIngredients(): Call<IngredientsReturnModel>
+
     // TODO: serve call
+    //fun
 }
+
+// result generated from /json
+
+data class IngredientsReturnModel(val juices: List<Juices>?, val drinks: List<Drinks>?, val ingredients: List<Ingredients>?, val alcohols: List<Any>?)
+
+data class Drinks(val id: String?, val label: String?, val type: String?, val color: String?, val opacity: Number?, val imageUrl: String?)
+
+data class Ingredients(val id: String?, val label: String?, val type: String?, val weight: Number?, val imageUrl: String?, val sprites: List<String>?)
+
+data class Juices(val id: String?, val label: String?, val type: String?, val color: String?, val opacity: Number?, val imageUrl: String?)
