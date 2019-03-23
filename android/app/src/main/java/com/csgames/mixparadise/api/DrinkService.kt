8262054fee008695411341c1a2ecac3d1adf3603
Dan Sirbu <dsirb066@gmail.com>
2019-03-23 14:47:43 -0400

    Added serving review api

--- android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
+++ android/app/src/main/java/com/csgames/mixparadise/api/DrinkService.kt
@@ -2,17 +2,17 @@ package com.csgames.mixparadise.api
 
 import retrofit2.Call
 import retrofit2.http.GET
-
+import retrofit2.http.Query
 
 
 interface DrinkService {
-    // TODO: get ingredients
+    //TODO remove key from here
 
     @GET("ingredients?key=asdasdsdsfds")
     fun getIngredients(): Call<IngredientsReturnModel>
 
-    // TODO: serve call
-    //fun
+    @GET("serve?key=asdasdsdsfds")
+    fun serve(@Query("id") id: String, @Query("qty") quantity: Int) : ServingReview
 }
 
 // result generated from /json
@@ -24,3 +24,10 @@ data class Drinks(val id: String?, val label: String?, val type: String?, val co
 data class Ingredients(val id: String?, val label: String?, val type: String?, val weight: Number?, val imageUrl: String?, val sprites: List<String>?)
 
 data class Juices(val id: String?, val label: String?, val type: String?, val color: String?, val opacity: Number?, val imageUrl: String?)
+
+
+
+data class ServingReview(val rating: Rating, val review: Review)
+
+data class Rating(val note: Integer, val comment: String)
+data class Review(val taste: Int, val volume: Int, val strength: Int)
